db17da7e611fbd3c1c0573b34ecc2dc6b8bd4a7e
Deepanshupundir <deepanshurana96900@gmail.com>
2024-04-02 12:36:46 +0530

    Work on js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello Hii")
+var a =15;
+var b =10.5
+var c = -10
+
+
+var d = true
+ var e = false
+var f = 'Hello'
+var g = "Hello"
+ var h = `Ducat
+  Educaion`
+  console.log(a, b, c ,d , e, f, g, h,);
