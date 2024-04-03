c1e0185c356e0e84503670b1d90eb81a906ec1c3
Deepanshupundir <deepanshurana96900@gmail.com>
2024-04-03 15:50:29 +0530

    new code commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var a =15;
+/*var a =15;
 var b =10.5
 var c = -10
 
@@ -9,4 +9,34 @@ var f = 'Hello'
 var g = "Hello"
  var h = `Ducat
   Educaion`
-  console.log(a, b, c ,d , e, f, g, h,);
+  console.log(a, b, c ,d , e, f, g, h,); */
+
+//   console.log(Number(123))
+//  console.log(parseInt(12.12))
+// console.log(parseFloat(12.34))
+
+// console.log(Math.floor(12.9))
+
+// console.log(Math.ceil(12.9))
+
+// console.log(Math.round(12.1))
+
+
+//  prompt run with browser becouse prompt is not a part of node
+// // prompt pass a string 
+// var a = prompt("Enter the First number:")
+// var b = prompt("Enter the Second number:")
+// var sum = a+b
+
+// document.write(`${a} + ${b} = ${sum}`)
+
+
+
+
+
+//number pass  a add two number
+// var a =Number(prompt("Enter the First number:"))
+// var b =Number(prompt("Enter the Second number:"))
+// var sum = a+b
+
+// document.write(`${a} + ${b} = ${sum}`)
