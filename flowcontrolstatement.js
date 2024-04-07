b833f3183b77702c9f93e3e75a846b4a18d6a58c
Deepanshupundir <deepanshurana96900@gmail.com>
2024-04-07 08:26:43 +0530

    more add

---
+++ flowcontrolstatement.js
@@ -0,0 +1,85 @@
+
+// if statement
+
+// if(true){
+// console.log("Hello Word1")
+// console.log("Hello Word2")
+// console.log("Hello Word3")
+// }                                                          
+
+// console.log("Hello Word4")         //All are run becouse independent
+// console.log("Hello Word5")
+// console.log("Hello Word6")
+
+ // if(!false)
+// console.log("Hello Word1")
+// console.log("Hello Word2")
+// console.log("Hello Word3")
+// console.log("Hello Word4")       
+// console.log("Hello Word5")
+// console.log("Hello Word6")
+
+// if else statement
+ //  when if  is false the else on work 
+
+// if (false) {
+//     console.log("Hello Word1")
+//     console.log("Hello Word2")
+//     console.log("Hello Word3")
+// }
+//  else{                       
+    //  when if  is false the else on work 
+// console.log("Hello Word4")        
+// console.log("Hello Word5")
+// console.log("Hello Word6")
+//  }
+
+
+//  var num = Number(prompt("Enter the Number"))
+// if (num % 2 ==0)
+// document.write(`${num} num is even`)
+// else
+// document.write(`${num} num is odd`)
+
+
+//Nested if else statement
+
+// var a = Number(prompt("Enter the First Number:"))
+// var b = Number(prompt("Enter the second Number:"))
+// var c = Number(prompt("Enter the third Number:"))
+
+// if (a < b) {
+//     if (a < c)
+
+//         document.write(`${a}is smallest`)
+//     else
+//         document.write(`${c}is smallest`)
+
+// }
+
+// else {
+//     if (b < c)
+//         document.write(`${b}is smallest `)
+//     else
+//         document.write(`${c}is smallest `)
+// }
+
+
+var num = Number(prompt("Enter the  first  Number"))
+var num = Number(prompt("Enter the  second Number"))
+var num = Number(prompt("Enter the third Number"))
+if(a < b){
+    if(a < c)
+    document.write(`${a} is greater`)
+else
+    document.write(`${c} is greater`)
+}
+else {
+    if(b < c)
+    document.write(`${b} is greater`)
+else
+    document.write(`${c} is greater`)
+}
+
+
+
