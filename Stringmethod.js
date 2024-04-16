86c1286109d7619984c64d6763bebda5f69f562f
Deepanshupundir <deepanshurana96900@gmail.com>
2024-04-16 17:01:58 +0530

    editingin commit

---
+++ Stringmethod.js
@@ -0,0 +1,18 @@
+//string constant
+
+// var a = 'deepu'
+// var b = "ducat"
+// var c = ` Noida 
+//  sec16
+//  It sector`
+
+// console.log("a typeof a")
+// console.log("b typeof b")
+// console.log("c typeof c")
+
+//strinf using for loops
+var a = "ducat is education"
+for (let i =0; i<a .length; i++){
+    console.log(a[i]);
+    i++
+}
