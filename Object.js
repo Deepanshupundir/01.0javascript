1f245bb13eb0367bb237fa932ffb36d10e8e80ad
Deepanshupundir <deepanshurana96900@gmail.com>
2024-04-20 23:35:08 +0530

    fun/ method in object commit

---
+++ Object.js
@@ -0,0 +1,92 @@
+//object literal
+
+// let emp ={
+//     id: 1001,
+//     name:"Deepanshu Rana",
+//     Dsg: "Web developer",
+//     salary:50000,
+//     city: "Noida",
+//     State: "UP",
+// }
+// console.log(emp);
+
+// Empty Object Literal
+
+// var emp = {}
+// emp.id = 1001
+// emp["name"] = 'Deepanshu Rana, Any '
+// emp['dsg'] = " Web developer"
+// emp [ `salary`] =  50000
+// emp ["city"] =  `Noida`
+// emp ["State"] = "UP"
+// console.log(emp);
+
+
+
+//   New object 
+
+// var emp = Object()
+// emp.id = 1001
+// emp["name"] = 'Deepanshu Rana, Any '
+// emp['dsg'] = " Web developer"
+// emp [ `salary`] =  50000
+// emp ["city"] =  `Noida`
+// emp ["State"] = "UP"
+// console.log(emp);
+
+// Acessing object value with help of keys
+//  var emp = Object()
+
+// console.log(emp);
+// console.log(emp.id);
+// console.log(emp['name']);
+// console.log(emp["dsg"]);
+// console.log(emp["salary"]);
+// console.log(emp["state"]);
+
+
+ // Acessing object value through loops
+
+//  for(let key in emp){
+//     console.log(`${key}=${emp[key]}`);
+// }         
+
+//  Adding on  item in object existing
+
+//  var  emp = Object()
+//  console.log(emp);
+// emp.email="pundir123@gmil.com"
+// emp["Phone"] = "9690056310"
+// emp[`Department`] = `IT`
+// emp["age"]  =   25
+// console.log(emp);
+
+
+//  Deleting object item 
+//   var emp = Object()
+// delete emp.id
+// delete emp['name']
+// delete emp["dsg"]
+// delete emp[`salary`]
+// delete emp["state"]
+// console.log(emp);
+
+
+//  Nesting of Object
+
+// var emp = {
+// name: `Deepanshu Rana`,
+// dsg: " Web developer",
+// salary:50000,
+// side: ["html , css, js, jquary,bootstrap, react"],
+// active:"true",
+// address:{
+//     house: 1,
+//     street:5,
+//     nearby:"pnb bank",
+//      city:"noida",
+//      State:"Up",
+//      pin:2000221,
+// }
+// }
+// console.log(emp);
