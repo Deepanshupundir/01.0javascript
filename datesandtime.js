5bd9fe431153f350dc60b45c0287c12cbab0a77c
Deepanshupundir <deepanshurana96900@gmail.com>
2024-05-15 14:05:40 +0530

    SetmapandDatatype committed

--- datesandtime.js
+++ datesandtime.js
@@ -1,13 +1,15 @@
-// var a = new Date()
-// console.log(a);
+//  Date and time 
+// var a = new Date()        show actucal time  and real time and present date
+// console.log(a);    
 // console.log(a.toLocaleDateString());
 // console.log(a.toLocaleTimeString()); 
 
 //   2 style : new Date (years,month, days,hour,minutes,second,millisecond)
 // we have specify atleast two items
-// var a  = new Date (2024,7, 12,5,56,2000)
+// var a  = new Date (2024,7, 12,5,56,2000)   ant time cheak
 // console.log(a.toLocaleString());
-
+ 
+//console.log(a.toDateString());
 
 // 3 style : new date (millisecond)
 
@@ -18,3 +20,17 @@
 
 // var a = new Date("2024/8/12")
 // console.log(a.toLocaleString());
+
+
+//  getFullYear() show current time
+// var a = new Date()
+// console.log(a.getFullYear());
+// console.log(a.getMonth());
+// console.log(a.getDate());
+// console.log(a.getHours());
+// console.log(a.getMinutes());
+// console.log(a.getSeconds());
+// // console.log(a.getMilliseconds());
+// console.log(a.getUTCDate());
+// // console.log(a.getTime());
+// console.log(a.getDay());
