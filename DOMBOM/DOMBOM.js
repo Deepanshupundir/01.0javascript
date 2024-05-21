a8af1ec1c1ce64d88e4f4a4bc18ef4651e87ee3e
Deepanshupundir <deepanshurana96900@gmail.com>
2024-05-21 13:36:59 +0530

    Add more commit

---
+++ DOMBOM/DOMBOM.js
@@ -0,0 +1,70 @@
+// 1: Get Element by id selector
+// var p = document.getElementById("p1")
+
+// p.style.backgroundColor = "gray"
+// p.style.color = "white"
+// p.style.Align = "justify"
+// p.style.padding = "2px"
+// p.style.fontSize = "15px"
+
+
+// 2 Get Element by class selector
+// var item = document.getElementsByClassName("sample")
+// for (let p of item) {
+
+
+//         p.style.backgroundColor = "gray"
+//         p.style.color = "white"
+//         p.style.Align = "justify"
+//         p.style.padding = "2px"
+//         p.style.fontSize = "15px"
+// }
+
+//  3 Get Element by  tag selector
+
+// var item = document.getElementsByTagName("p2")
+// for (let p of item) {
+// p.style.backgroundColor = "gray"
+// p.style.color = "white"
+// p.style.Align = "justify"
+// p.style.padding = "2px"
+// p.style.fontSize = "15px"
+// }
+
+
+
+//  Get Element by quary selector
+
+
+// var item = document.getElementsByTquerySelector(".sample")
+// for (let p of item) {
+// p.style.backgroundColor = "gray"
+// p.style.color = "white"
+// p.style.Align = "justify"
+// p.style.padding = "2px"
+// p.style.fontSize = "15px"
+// }
+
+// 5 Get Elment by quary selector All
+
+
+// var item = document.getElementsByquerySelectorAll(["#p1","#p2","#p3","#p4"])
+// for (let p of item) {
+// p.style.backgroundColor = "gray"
+// p.style.color = "white"
+// p.style.Align = "justify"
+// p.style.padding = "2px"
+// p.style.fontSize = "15px"
+// }
+        // var items = document.querySelectorAll("p1 ")
+        // var items = document.querySelectorAll(".sample2")
+        // var items = document.querySelectorAll(["#p1","#p3","#p4"])
+        // for (let p of items) {
+        //     p.style.backgroundColor = "blue"
+        //     p.style.color = "white"
+        //     p.style.textAlign = "justify"
+        //     p.style.padding = "10px"
+        //     p.style.fontSize = "25px"
+        // }
+
+
